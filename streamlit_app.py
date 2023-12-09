46376ac4ab3d0b395aea16af5cd802bc8daa86cc
jackmyap <103487711+jackmyap@users.noreply.github.com>
2023-12-09 12:27:09 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,56 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 
-"""
-# Welcome to Streamlit!
+# Load your trained model (this assumes you have a trained model saved as a pickle file)
+# model = pd.read_pickle("/path/to/your/model.pkl")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Define the structure of the user input
+def user_input_features():
+    age = st.sidebar.number_input("Age", 18, 80, 30)
+    sex = st.sidebar.selectbox('Sex', ('male', 'female'))
+    cp = st.sidebar.selectbox('Chest Pain Type', ('typical angina', 'atypical angina', 'non-anginal pain', 'asymptomatic'))
+    trestbps = st.sidebar.slider('Resting Blood Pressure', 90, 200, 120)
+    chol = st.sidebar.slider('Serum Cholestoral in mg/dl', 100, 600, 300)
+    fbs = st.sidebar.radio('Fasting Blood Sugar > 120 mg/dl', ('True', 'False'))
+    restecg = st.sidebar.selectbox('Resting Electrocardiographic Results', ('normal', 'ST-T wave abnormality', 'left ventricular hypertrophy'))
+    thalach = st.sidebar.slider('Maximum Heart Rate Achieved', 70, 200, 150)
+    exang = st.sidebar.radio('Exercise Induced Angina', ('Yes', 'No'))
+    
+    # Convert categorical inputs to numeric values for the model
+    # This would involve transforming the user input to match the format that the model expects
+    # For example, if your model uses one-hot encoding for categorical data, you'd convert the input here
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+    data = {'age': age,
+            'sex': sex,
+            'cp': cp,
+            'trestbps': trestbps,
+            'chol': chol,
+            'fbs': fbs,
+            'restecg': restecg,
+            'thalach': thalach,
+            'exang': exang
+            }
+    features = pd.DataFrame(data, index=[0])
+    return features
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+# Set up the main app
+st.write("""
+# Heart Disease Prediction App
+This app predicts the **Probability of Heart Disease** based on your input parameters.
+""")
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+st.sidebar.header('User Input Parameters')
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+df = user_input_features()
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+# When 'Predict' is clicked, make the prediction and display it
+if st.button('Predict'):
+    prediction = model.predict(df)
+    st.write('Based on the input, the model predicts...')
+    st.write(prediction)
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+# You can also display the user input for verification
+st.subheader('User Input Parameters')
+st.write(df)
